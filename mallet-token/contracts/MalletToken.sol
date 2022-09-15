8cc7abbb6f1938ab7b543a2250277e1de2f25a8d
MitPete <99507140+MitPete@users.noreply.github.com>
2022-09-15 10:34:09 -0700

    Update MalletToken.sol

--- mallet-token/contracts/MalletToken.sol
+++ mallet-token/contracts/MalletToken.sol
@@ -1,5 +1,3 @@
-//Here we are creating an ERC 20 token importing the ERC20 standard and passing in
-//our constructor the intial supply and the ERC 20 name of out token and tickers(MLT)
 //SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.17;
@@ -11,12 +9,12 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
 contract MalletToken is ERC20Capped ,ERC20Burnable{
     address payable public owner;
-    uint256 public blockReward; 
-constructor(uint256 cap,uint256 reward) ERC20 ("MalletToken","MLT") ERC20Capped(cap * (10 ** decimals())) {//implements the cap supply 
-        //Create token (_mint) and give total supply to me 
+    uint256 public blockReward;
+    
+constructor(uint256 cap,uint256 reward) ERC20 ("MalletToken","MLT") ERC20Capped(cap * (10 ** decimals())) {
         owner = payable(msg.sender);
-        blockReward = reward * (10 ** decimals());//setting the block reward 
-        _mint(owner,70000000 * (10 ** decimals()));//This gives us 70 millions to owner with the 18 decimal places BN 
+        blockReward = reward * (10 ** decimals()); 
+        _mint(owner,70000000 * (10 ** decimals()));
     }
 
         function _mint(address account, uint256 amount) internal virtual override(ERC20Capped,ERC20) {
@@ -44,7 +42,7 @@ constructor(uint256 cap,uint256 reward) ERC20 ("MalletToken","MLT") ERC20Capped(
     function destroy() public onlyOwner{
         selfdestruct(owner);
     }
-    //only owner modiifer 
+    
     modifier onlyOwner {
         require(msg.sender == owner,"Only the owner can access this function");
         _;
